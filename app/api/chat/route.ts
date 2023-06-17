7bea6d47e7879088d992c412526f5f8d525f3215
westy12dan <westy12dan@gmail.com>
2023-06-17 14:21:54 +0800

    list

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,4 +1,8 @@
-import { Configuration, OpenAIApi } from 'openai-edge'
+import {
+  ChatCompletionRequestMessage,
+  Configuration,
+  OpenAIApi,
+} from 'openai-edge'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 
 export const runtime = 'edge'
@@ -12,6 +16,30 @@ const openai = new OpenAIApi(apiConfig)
 export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
   const { messages } = await req.json()
+
+  // Add system message
+  ;(messages as ChatCompletionRequestMessage[]).unshift({
+    role: 'system',
+    content: `
+You are an AI that converts a users list written in natural language into yaml formatted output. 
+Your response should only contain yaml output. An example of a list formatted as yaml:
+
+- name: Banana
+   quantity: 3
+- name: Apple
+   quantity: 1
+
+Your yaml should only only contain the fields name and quantity as show in the example above. 
+Exclude anything that appears to be a heading and not an item in the list. All quantities should 
+be numbers. If the quantity is specified as a number use the specified number. If the same item 
+is specified multiple times combine the quantities into one item. If it is specified in natural 
+language convert it to the equivalent number. For example "few" is 2, "couple" is 2, "several" 
+is 3, if unsure use 1 as the quantity. Quantity should be an integer not a decimal.
+
+You should not add any information to your generated response that isn't mentioned in the users 
+message. Although you can fix capitalisation or spelling errors in the users message. 
+Ignore anything that appears to be a heading.`,
+  })
 
   // Request the OpenAI API for the response based on the prompt
   const response = await openai.createChatCompletion({
--- app/chat.tsx
+++ app/chat.tsx
@@ -1,33 +1,56 @@
 'use client'
 
 import { useChat } from 'ai/react'
+import { parse, stringify } from 'yaml'
 
 export default function Chat() {
   const { messages, input, handleInputChange, handleSubmit } = useChat({
     api: '/api/chat',
   })
 
+  const combinedAssistantMessages = messages.reduce((acc, m) => {
+    if (m.role === 'assistant') {
+      acc += `${m.content}\n`
+    }
+    return acc
+  }, '')
+
+  const lines = combinedAssistantMessages.split('\n')
+  let partial = ''
+  let validYaml = ''
+  for (let i = 0; i < lines.length; i++) {
+    const line = lines[i]
+    if (line.startsWith('  quantity: ')) {
+      validYaml += partial + '\n' + line
+      partial = ''
+    } else {
+      partial = partial + '\n' + line
+    }
+  }
+  validYaml = validYaml.replace(/^\n+|\n+$/g, '')
+
+  const data: { name: string; quantity: number }[] = parse(validYaml)
   return (
     <div>
       <ul>
-        {messages.map((m, index) => (
-          <li key={index}>
-            {m.role === 'user' ? 'User: ' : 'AI: '}
-            {m.content}
-          </li>
-        ))}
+        {data?.map((item, i) => {
+          return (
+            <li key={i}>
+              {typeof item.quantity === 'number'
+                ? `${item.quantity} ${item.name}`
+                : item.name}
+            </li>
+          )
+        })}
       </ul>
 
-      <form
-        onSubmit={handleSubmit}
-        className="flex space-x-2 items-stretch w-full"
-      >
-        <input
+      <form onSubmit={handleSubmit} className="flex space-x-2 items-end w-full">
+        <textarea
           className="border-gray-400 border-2 rounded-md px-2 py-1 flex-1"
           value={input}
           onChange={handleInputChange}
-          aria-label="Say something..."
-          placeholder="Say something..."
+          aria-label="Your list..."
+          placeholder="Your list..."
         />
         <button
           type="submit"
@@ -39,3 +62,11 @@ export default function Chat() {
     </div>
   )
 }
+
+const example = `
+Groceries:
+3 Bannans
+4 Apples
+Soy sauce
+a few milks
+`
